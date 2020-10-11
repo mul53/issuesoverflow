40851e14dfe353540de35aec3adbf95285b48365
Obikaonu Samuel <samuelchibuike22@gmail.com>
2020-10-11 22:46:51 +0100

    add loader before fetch isssues

--- client/src/components/IssuesList.js
+++ client/src/components/IssuesList.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Columns } from "react-bulma-components";
 import Paginate from "react-paginate";
+import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
 
 import Issue from "./Issue";
 import { fetchIssues } from "../services/issuesService";
@@ -8,22 +10,33 @@ import { fetchIssues } from "../services/issuesService";
 const IssuesList = () => {
   const [issues, setIssues] = useState([]);
   const [pageCount, setPageCount] = useState(0);
-
+  const [loading, setLoading] = useState(false)
+  const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  z-index:99999;
+`;
+ 
   useEffect(() => {
+    setLoading(true)
     fetchIssues()
       .then((data) => data.json())
       .then((data) => {
         setPageCount(data.totalPages);
         setIssues(data.data);
+        setLoading(false);
       });
   }, [pageCount]);
 
   const handlePageClick = ({ selected }) => {
+    setLoading(true);
     fetchIssues(selected + 1)
       .then((data) => data.json())
       .then((data) => {
         setPageCount(data.totalPages);
         setIssues(data.data);
+        setLoading(false);
       });
   };
 
@@ -32,7 +45,7 @@ const IssuesList = () => {
       <Columns>
         {issues.map((issue) => (
           <Columns.Column size={4} mobile={{ size: 12 }}>
-            <Issue issue={issue} />
+            <Issue  issue={issue} />
           </Columns.Column>
         ))}
       </Columns>
@@ -45,10 +58,18 @@ const IssuesList = () => {
         marginPagesDisplayed={2}
         pageRangeDisplayed={5}
         onPageChange={handlePageClick}
-        containerClassName={"pagination is-centered"}
+        containerClassName={"pagination"}
         subContainerClassName={"pages pagination"}
         activeClassName={"active"}
       />
+      <div className="sweet-loading is-center">
+        <ClipLoader
+          css={override}
+          size={150}
+          color={"#123abc"}
+          loading={loading}
+        />
+      </div>
     </ul>
   );
 };
